2a59ecfd03222a23e8a541517ed291ab0544eb50
Lalkeen <69041664+Lalkeen@users.noreply.github.com>
2024-09-30 20:49:08 +0300

    Update telegrambot.py
    теперь тут тоггл

--- telegrambot.py
+++ telegrambot.py
@@ -10,29 +10,17 @@ tv_id = 'LGwebOSTV' # Тут название девайса
 @bot.message_handler(commands=['start'])
 def salam(message) -> None:
     bot.send_message(message.chat.id, f'Salam!')
-    bot.send_message(message.chat.id, f'Команды: /on_vpn_on_tv /off_vpn_on_tv')
+    bot.send_message(message.chat.id, f'Команды: /toggle_vpn_on_tv')
 
 
-@bot.message_handler(commands=['on_vpn_on_tv'])
-def on_vpn_on_tv(message) -> None:
+@bot.message_handler(commands=['toggle_vpn_on_tv'])
+def toggle_vpn_on_tv(message) -> None:
     bot.send_message(message.chat.id, f'Работаем, братья')
 
     try:
-        commands.move_to_vpn(tv_id) # в скобки передаем устройство
-        bot.send_message(message.chat.id, f'VPN на телевизоре включен')
+        bot.send_message(message.chat.id, f'{commands.toggle(tv_id)}')
     except:
         commands.exit()
-        bot.send_message(message.chat.id, f'Ты ошибся, парень')
-
-
-@bot.message_handler(commands=['off_vpn_on_tv'])
-def off_vpn_on_tv(message) -> None:
-    bot.send_message(message.chat.id, f'Работаем, братья')
-    try:
-        commands.move_back(tv_id) # в скобки передаем устройство
-        bot.send_message(message.chat.id, f'VPN на телевизоре выключен')
-    except:
-
         bot.send_message(message.chat.id, f'Ты ошибся, парень')
 
 
