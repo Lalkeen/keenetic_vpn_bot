dd66103b0fd1bae67f5f238c9f044e52fa70f915
Lalkeen <69041664+Lalkeen@users.noreply.github.com>
2024-10-01 15:45:47 +0300

    now with buttons

--- telegrambot.py
+++ telegrambot.py
@@ -6,35 +6,37 @@ bot = telebot.TeleBot('BOT-TOKEN')
 
 host = '192.168.1.1'
 password = 'admin'
-tv_id = 'LGwebOSTV'
-tv_mac = '11:11:11:11:11:11'
+maclist = {'LGwebOSTV': '11:11:11:11:11:11', 'Mobile': '11:11:11:11:11:11', 'PS5':'11:11:11:11:11:11'}
+
+
+markup = InlineKeyboardMarkup()
+for button_id, button_text in maclist.items():
+    markup.add(telebot.types.InlineKeyboardButton(text=button_id, callback_data=button_text))
 
 
 @bot.message_handler(commands=['start'])
 def salam(message) -> None:
     bot.send_message(message.chat.id, f'Salam!')
-    bot.send_message(message.chat.id, f'Команды: /api_off_on_tv /api_on_on_tv')
+    bot.send_message(message.chat.id, "Select option:", reply_markup=markup)
 
-@bot.message_handler(commands=['api_on_on_tv'])
-def api_on_on_tv(message) -> None:
-    bot.send_message(message.chat.id, f'Работаем, братья')
 
-    try:
-        bot.send_message(message.chat.id, f'{commands.api_on(tv_mac, host, password)}')
-    except:
-        commands.exit()
-        bot.send_message(message.chat.id, f'Ты ошибся, парень')
-
-@bot.message_handler(commands=['api_off_on_tv'])
-def api_on_on_tv(message) -> None:
-    bot.send_message(message.chat.id, f'Работаем, братья')
-
-    try:
-        bot.send_message(message.chat.id, f'{commands.api_off(tv_mac, host, password)}')
-    except:
-        commands.exit()
-        bot.send_message(message.chat.id, f'Ты ошибся, парень')
-
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data in maclist.values():
+        new_markup = InlineKeyboardMarkup()
+        new_markup.add(types.InlineKeyboardButton(text='On', callback_data=f'on_{call.data}'))
+        new_markup.add(types.InlineKeyboardButton(text='Off', callback_data=f'off_{call.data}'))
+        bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=new_markup)
+    elif call.data.startswith('on_'):
+        mac = call.data.split('_')[1]
+        commands.api_on(mac, host, password)
+        bot.delete_message(call.message.chat.id, call.message.message_id)
+        bot.send_message(call.message.chat.id, "Сделано! Что дальше?", reply_markup=markup)
+    elif call.data.startswith('off_'):
+        mac = call.data.split('_')[1]
+        commands.api_off(mac, host, password)
+        bot.delete_message(call.message.chat.id, call.message.message_id)
+        bot.send_message(call.message.chat.id, "Сделано! Что дальше?", reply_markup=markup)
 
 
 bot.infinity_polling(timeout=10, long_polling_timeout = 5)
