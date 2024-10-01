b7fcde3ecbd2af7ead0ca43334d28651a629f016
Lalkeen <69041664+Lalkeen@users.noreply.github.com>
2024-10-01 16:34:51 +0300

    now with wake on lan

--- commands.py
+++ commands.py
@@ -2,24 +2,21 @@ import keenetic
 
 def api_on(mac, host, password):
     addr = '/rci/ip/hotspot/host'
-    json = {"mac":'', 'permit': 'true', 'policy': 'Policy0'}
-    json['mac'] = mac
+    json = {"mac": mac, 'permit': 'true', 'policy': 'Policy0'}
     router = keenetic.Router(username="admin", password=password, host=host, port=80)
-    for device in router.connected_devices:
-        if device['mac'] == json['mac']:
-            print("______________________________")
-            print(device)
     router.post(addr, json)
-    return "На устройстве включен ВПН"
+
 
 def api_off(mac, host, password):
     addr = '/rci/ip/hotspot/host'
-    json = {"mac": '', 'permit': 'true', 'policy': False}
-    json['mac'] = mac
+    json = {"mac": mac, 'permit': 'true', 'policy': False}
     router = keenetic.Router(username="admin", password=password, host=host, port=80)
-    for device in router.connected_devices:
-        if device['mac'] == json['mac']:
-            print("______________________________")
-            print(device)
     router.post(addr, json)
-    return "На устройстве выключен ВПН"
+
+
+def wake(mac, host, password):
+    addr = '/rci/ip/hotspot/wake'
+    json = {'mac': mac}
+    router = keenetic.Router(username="admin", password=password, host=host, port=80)
+    router.post(addr, json)
+    
