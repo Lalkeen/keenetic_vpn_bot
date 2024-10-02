a37f9d9f6bccf064f7bae58452a177824e7b5523
Lalkeen <69041664+Lalkeen@users.noreply.github.com>
2024-10-02 13:17:02 +0300

    added registration

--- telegrambot.py
+++ telegrambot.py
@@ -11,22 +11,23 @@ password = 'admin'
 
 with open('alllists.json', 'r') as f:
     alllists = json.load(f)
-    
-maclist = alllists['maclist'] # список устройств
-wollist = alllists['wollist'] # wake on lan list - возможность будить устройство 
-whitelist = alllists['whitelist'] # вайтлист пользователей. получить айди командой /auth
+
+maclist = alllists['maclist']
+wollist = alllists['wollist']
+whitelist = alllists['whitelist']
 
 markup = InlineKeyboardMarkup()
 for button_id, button_text in maclist.items():
     markup.add(telebot.types.InlineKeyboardButton(text=button_id, callback_data=button_text))
 
+
 @bot.message_handler(commands=['start'])
 def salam(message) -> None:
     if message.chat.id in whitelist:
-        bot.send_message(message.chat.id, f'Salam!')
-        bot.send_message(message.chat.id, "Select option:", reply_markup=markup)
+        bot.send_message(message.chat.id, "Выбери устройство, затем действие:", reply_markup=markup)
     else:
         bot.send_message(message.chat.id, f'Ты ошибся дверью, браток')
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
@@ -52,10 +53,47 @@ def callback_query(call):
         commands.wake(mac, host, password)
         bot.delete_message(call.message.chat.id, call.message.message_id)
         bot.send_message(call.message.chat.id, "Сделано! Что дальше?", reply_markup=markup)
+    elif call.data.startswith('addtowol_'):
+        mac = call.data.split('_')[1]
+        wollist.append(mac)
+        alllists["wollist"] = list(wollist)
+        with open('alllists.json', 'w') as file:
+            json.dump(alllists, file)
+        bot.delete_message(call.message.chat.id, call.message.message_id)
+        bot.send_message(call.message.chat.id, 'Устройство добавлено в список')
+        salam(call.message)
+    elif call.data == 'notaddtowol':
+        bot.delete_message(call.message.chat.id, call.message.message_id)
+        bot.send_message(call.message.chat.id, 'Устройство не будет добавлено в список')
+        salam(call.message)
+
+
+@bot.message_handler(commands=['reg'])
+def reg(message) -> None:
+    if message.chat.id in whitelist:
+        bot.send_message(message.chat.id, f'Для добавления устройства в список отправь сообщение типа "Имя_устройства МАК_адрес", например: TV 11:11:11:11:11:11')
+        bot.register_next_step_handler(message, add_device)
+
+
+def add_device(message) -> None:
+    try:
+        device_name, mac_address = message.text.split()
+        maclist[device_name] = mac_address
+        alllists["maclist"] = dict(maclist)
+        with open('alllists.json', 'w') as file:
+            json.dump(alllists, file)
+        bot.send_message(message.chat.id,
+                         f'Устройство {device_name} с МАК-адресом {mac_address} добавлено в список.')
+        wol_markup = InlineKeyboardMarkup()
+        wol_markup.add(types.InlineKeyboardButton(text='Да', callback_data=f'addtowol_{mac_address}'))
+        wol_markup.add(types.InlineKeyboardButton(text='Нет', callback_data='notaddtowol'))
+        bot.send_message(message.chat.id, 'Добавить в список wake-on-lan?', reply_markup=wol_markup)
+    except ValueError:
+        bot.send_message(message.chat.id,
+                         'Неверный формат сообщения. Пожалуйста, отправьте сообщение типа "Имя_устройства МАК_адрес".')
 
 @bot.message_handler(commands=['auth'])
 def auth(message) -> None:
     bot.send_message(message.chat.id, message.chat.id)
 
 bot.infinity_polling(timeout=10, long_polling_timeout = 5)
-
